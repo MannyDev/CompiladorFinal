df41e73248ea5979539cdc6afbee06da0324a95f
Manny <many.jose@hotmail.com>
2013-12-25 15:47:37 -0600

    to windows

---
+++ file2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int a, b=3; /* foobar */
+    a = b;
+    a+=2; // equivalent to a=a+2 :)
+    scanf("%i",&b);
+    puts("\a/a");
+    printf("hola manny \a");
+    return 0;
+}
---
+++ mm.c
@@ -0,0 +1,9 @@
+int x, y;
+
+int main(){
+    
+    x = 3;
+    y = 1;
+    
+    return x + y;
+}
